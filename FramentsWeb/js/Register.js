6ead734d0f5678bf2ffa806302d87e2eccc9dbbe
alexis14kl <rapalexism@gmail.com>
2022-08-08 03:59:32 -0500

    ok

--- FramentsWeb/js/Register.js
+++ FramentsWeb/js/Register.js
@@ -7,21 +7,147 @@
             var profile = $('#profile').val();
             var valid = $('#valid').val();
             if(name.trim()  === ''){
-                alert("nombre esta vacio" );
+                Swal.fire(
+            {
+            html: '<p style="color:red;">Nombre obligatorio <i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
                 return false;
             }else if(lastname.trim() === ''){
-                alert("lastname vacio");
+                Swal.fire(
+            {
+            html: '<p style="color:red;">Apellidos obligatorio <i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
             }else if(email.trim() === ''){
-                alert("correo vacio");
+                Swal.fire(
+            {
+            html: '<p style="color:red;">Correo obligatorio <i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
             }else if(password.trim() === ''){
-                alert("pass vacio");
+               Swal.fire(
+            {
+            html: '<p style="color:red;">contrasena obligatoria<i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
             }else if(repassword.trim() === ''){
-                alert('res vacio');
+                Swal.fire(
+            {
+            html: '<p style="color:red;">Repiticion de la Contrasena obligatorio<i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
             }else if(profile == "0") {
-                alert("seleccione un perfil valido x el sistema" + profile);
+               Swal.fire(
+            {
+            html: '<p style="color:red;">Seleccione un perfil valido <i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
             }else if(valid == "válido"){
                 if(password != repassword){
-                    alert("las contrasenas no son =");
+                    Swal.fire(
+            {
+            html: '<p style="color:red;">Las Contrasenas no coiciden <i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
                 }else{
                     $.get('http://localhost:8080/validEmailS',{email:email}, function(data) {
                 var cadenadata = String(data);
@@ -31,18 +157,96 @@
                                 email:email,
                                 password:password,profile:profile},
                          function(register) {
-                            alert(register);
+                            if(register = 'register'){
+
+                                Swal.fire(
+            {
+            html: '<p style="color:green;">Usuario Registrado <i class="fa fa-user-plus" aria-hidden="true"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
+
+                            }else{
+                                alert("algo esta mal");
+                            }
                         });
                     }else if(cadenadata == 'yes'){
-                        alert("el correo ya existe Rwgistrado intenta con otro");
+                        Swal.fire(
+            {
+            html: '<p style="color:red;">El correo ya existe Registrado <i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
                     }
          }).catch(
     function error(){
-        alert("lo siento el servicio no esta disponible x el momento");
+        Swal.fire(
+            {
+            html: '<p style="color:red;">Lo siento el servidor esta en mantenimiento <i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
     }
     );;
                 }
             }else if(valid == "incorrecto"){
-                alert("email incorrecto format");
+                Swal.fire(
+            {
+            html: '<p style="color:red;">el Formato del correo no es valido<i class="fa fa-times"></i></p>',
+            backdrop: true,
+            maxlength: 1,
+            toast: true,//alert mas peque
+            popup: 'swal2-show',
+  backdrop: 'swal2-backdrop-show',
+  icon: 'swal2-icon-show',
+  confirmButton: false,
+  cancelButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  showConfirmButton: false,
+            position: 'top-end',didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }}
+            );
             }
         }
